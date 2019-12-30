c57d4396ce31d0fa547068470ec7a839efc5ca20
Chris Iverson <cj.no.one@gmail.com>
2019-12-30 13:28:15 -0600

    Making changed to make it easier to add commands later.

--- routes/command.js
+++ routes/command.js
@@ -2,8 +2,7 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+function activate() {
   const { spawnSync } = require('child_process');
   const pinOn = spawnSync('gpio', ['-g','write','4','1']);
   fs.writeFile("testFile", "1", function(err){
@@ -22,8 +21,25 @@ router.get('/', function(req, res, next) {
 		console.log("Button reset!");
 	  });
   }, 1000);
+  
+}
 
-  res.render('activate', { title: 'Activated' });
+/* GET home page. */
+router.get('/:action', function(req, res, next) {
+    var action = req.params['action'];
+	var title = "";
+
+	if(action === "activate") {
+		console.log("Command activate received.");
+		activate();
+		title = "Door Activated";
+    }
+	else {
+		console.log("Unknown command "+action);
+		title = "Unknown Command";
+	}
+
+    res.render('command', { title: title, action: action });
 });
 
 module.exports = router;
